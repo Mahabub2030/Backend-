dc5d32b836f70312850f0bb9b64766a6c4ba31f8
Mahaub Alam <mahabubalom72091@gmail.com>
2025-07-18 13:09:51 +0300

    added all code with jwt

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
 import httpStatus from "http-status-codes";
 import bcryptjs from "bcryptjs";
-
+import  jwt  from "jsonwebtoken";
 
 const credentialsLogin = async (payload: Partial<IUser>) =>{
     const {email,password} = payload;
@@ -18,10 +18,16 @@ const credentialsLogin = async (payload: Partial<IUser>) =>{
     if (!isPasswordMatched) {
         throw new AppError(httpStatus.BAD_REQUEST, "Incorrect Password")
     }
+ const jwtPayload = {
+        userId: isUserExist._id,
+        email: isUserExist.email,
+        role: isUserExist.role
+     }
+    const accessToken = jwt.sign(jwtPayload,"secret",{expiresIn:"1d"})
 
     // const {password, ...rest} = isUserExist;
     return {
-        email:isUserExist.email
+        accessToken
     }
 }
 
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,15 +1,44 @@
-
-
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { UserControllers } from "./user.controller";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { createUserZodSchema } from "./user.validation";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import AppError from "../../errorHelpers/AppError";
+import { Role } from "./user.interface";
 
+const router = Router();
 
-const router = Router()
+router.post(
+  "/register",
+  validateRequest(createUserZodSchema),
+  UserControllers.createUser
+);
+router.get(
+  "/all-users",
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const accessToken = req.headers.authorization;
+      const verifyToken = jwt.verify(accessToken, "secret");
+      
+      if (!accessToken) {
+        throw new AppError(403, "No Token Recive");
+      }
+      if (!verifyToken) {
+        throw new AppError(403, `you no have permt${verifyToken}`);
+        console.log(verifyToken);
+      }
 
-router.post("/register",validateRequest(createUserZodSchema) ,UserControllers.createUser)
-router.get("/all-users",UserControllers.getAllUsers)
+      if ((verifyToken as JwtPayload).role !== Role.ADMIN || Role.SUPER_ADMIN) {
+        throw new AppError(403, "you no have permt");
+      }
+      console.log(verifyToken);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  },
+  UserControllers.getAllUsers
+);
 
 // /api/v1/user/:id
-export const UserRoutes = router
+export const UserRoutes = router;
