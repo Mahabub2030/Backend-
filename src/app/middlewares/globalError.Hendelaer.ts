1263077ed3a92b95fb6e628517a79a49ed8cb542
Mahaub Alam <mahabubalom72091@gmail.com>
2025-07-20 12:44:26 +0300

    added all autitication

--- src/app/middlewares/globalError.Hendelaer.ts
+++ src/app/middlewares/globalError.Hendelaer.ts
@@ -4,17 +4,45 @@ import { envVars } from "../config/env";
 import AppError from "../errorHelpers/AppError";
 
 export const globalErrorHandeler = (
+
+
   err: any,
   req: Request,
   res: Response,
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction
 ) => {
+
+  console.log(err)
   let statusCode = 500;
 
   let message = "Something went wrong from gobal error";
+// dulicate Error
+if (err.code === 11000) {
+  console.log('Duplicate Error', err.message);
+  const matchedArray = err.message?.match(/"([^"]*)"/);
+  message = `${matchedArray[1]} alredy exitsts!!`;
+  statusCode = 400;
+  // objectID Error cush Error
+} else if(err.name === "CastError"){
+  statusCode =400
+  message="Invalid MongoDb Id,Please check"
+}
+// Zode Error
 
-  if(err instanceof AppError){
+ 
+
+
+    //Mongoose Validation Error
+else if (err.name === "ValidationError") {
+  statusCode = 400;
+  const errors = Object.values(err.errors);
+  console.log(errors)
+  message = "Validation erro";
+}
+
+
+ else if(err instanceof AppError){
     statusCode = err.statusCode
     message = err.message
   } else if (err instanceof Error){
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -10,41 +10,41 @@ import { envVars } from "../../config/env";
 import { createNewAccessTokenWithRefreshToken, createUserTokens } from "../../../utils/userTokens";
 import { JwtPayload } from "jsonwebtoken";
 
-const credentialsLogin = async (payload: Partial<IUser>) =>{
+// const credentialsLogin = async (payload: Partial<IUser>) =>{
 
 
-    const {email,password} = payload;
+//     const {email,password} = payload;
 
-      const isUserExist = await User.findOne({ email });
+//       const isUserExist = await User.findOne({ email });
 
-  if (!isUserExist) {
-        throw new AppError(httpStatus.BAD_REQUEST, "Email does not exist")
-    }
-  const isPasswordMatched = await bcryptjs.compare(password as string ,isUserExist.password as string)
+//   if (!isUserExist) {
+//         throw new AppError(httpStatus.BAD_REQUEST, "Email does not exist")
+//     }
+//   const isPasswordMatched = await bcryptjs.compare(password as string ,isUserExist.password as string)
 
-    if (!isPasswordMatched) {
-        throw new AppError(httpStatus.BAD_REQUEST, "Incorrect Password")
-    }
-//  const jwtPayload = {
-//         userId: isUserExist._id,
-//         email: isUserExist.email,
-//         role: isUserExist.role
-//      }
-//     // const accessToken = jwt.sign(jwtPayload,"secret",{expiresIn:"1d"})
-//     const accessToken = generateToken(jwtPayload,envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES)
+//     if (!isPasswordMatched) {
+//         throw new AppError(httpStatus.BAD_REQUEST, "Incorrect Password")
+//     }
+// //  const jwtPayload = {
+// //         userId: isUserExist._id,
+// //         email: isUserExist.email,
+// //         role: isUserExist.role
+// //      }
+// //     // const accessToken = jwt.sign(jwtPayload,"secret",{expiresIn:"1d"})
+// //     const accessToken = generateToken(jwtPayload,envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES)
 
-//    const refreshToken =generateToken(jwtPayload, envVars.JWT_REFRESH_SECRET,envVars.JWT_REFRESH_EXPIRES)
+// //    const refreshToken =generateToken(jwtPayload, envVars.JWT_REFRESH_SECRET,envVars.JWT_REFRESH_EXPIRES)
 
-//    delete isUserExist.password;
-  const userTokens = createUserTokens(isUserExist)
+// //    delete isUserExist.password;
+//   const userTokens = createUserTokens(isUserExist)
 
-const {password:pass, ...rest} = isUserExist.toObject()
+// const {password:pass, ...rest} = isUserExist.toObject()
 
-    // const {password, ...rest} = isUserExist;
-    return {
-        accessToken:userTokens.accessToken,refreshToken:userTokens.refreshToken,user:rest
-    }
-}
+//     // const {password, ...rest} = isUserExist;
+//     return {
+//         accessToken:userTokens.accessToken,refreshToken:userTokens.refreshToken,user:rest
+//     }
+// }
 const getNewAccessToken = async (refreshToken: string) => {
     const newAccessToken = await createNewAccessTokenWithRefreshToken(refreshToken)
 
@@ -70,7 +70,7 @@ const resetPassword = async (oldPassword: string, newPassword: string, decodedTo
 
 
 export const AuthServices = {
-    credentialsLogin,
+    // credentialsLogin,
     getNewAccessToken,
       resetPassword
     
