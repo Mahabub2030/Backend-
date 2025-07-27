4b9b0d48ae0af8099caaeee87eebb72cce49339d
Mahaub Alam <mahabubalom72091@gmail.com>
2025-07-27 14:34:35 +0300

    added user set-password to db

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -6,11 +6,10 @@ import httpStatus from "http-status-codes";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { envVars } from "../../config/env";
 import AppError from "../../errorHelpers/AppError";
-import { sendEmail } from "../../utils/sendEmail";
-import { createNewAccessTokenWithRefreshToken } from "../../utils/userTokens";
 import { IAuthProvider, IsActive } from "../user/user.interface";
 import { User } from "../user/user.model";
-
+import { createNewAccessTokenWithRefreshToken } from "../../../utils/userTokens";
+import { sendEmail } from "../../../utils/sendEmail";
 
 // const credentialsLogin = async (payload: Partial<IUser>) => {
 //     const { email, password } = payload;
@@ -50,127 +49,149 @@ import { User } from "../user/user.model";
 
 // }
 const getNewAccessToken = async (refreshToken: string) => {
-    const newAccessToken = await createNewAccessTokenWithRefreshToken(refreshToken)
+  const newAccessToken = await createNewAccessTokenWithRefreshToken(
+    refreshToken
+  );
 
-    return {
-        accessToken: newAccessToken
-    }
+  return {
+    accessToken: newAccessToken,
+  };
+};
+const resetPassword = async (
+  payload: Record<string, any>,
+  decodedToken: JwtPayload
+) => {
+  if (payload.id != decodedToken.userId) {
+    throw new AppError(401, "You can not reset your password");
+  }
 
-}
-const resetPassword = async (payload: Record<string, any>, decodedToken: JwtPayload) => {
-    if (payload.id != decodedToken.userId) {
-        throw new AppError(401, "You can not reset your password")
-    }
+  const isUserExist = await User.findById(decodedToken.userId);
+  if (!isUserExist) {
+    throw new AppError(401, "User does not exist");
+  }
 
-    const isUserExist = await User.findById(decodedToken.userId)
-    if (!isUserExist) {
-        throw new AppError(401, "User does not exist")
-    }
+  const hashedPassword = await bcryptjs.hash(
+    payload.newPassword,
+    Number(envVars.BCRYPT_SALT_ROUND)
+  );
 
-    const hashedPassword = await bcryptjs.hash(
-        payload.newPassword,
-        Number(envVars.BCRYPT_SALT_ROUND)
-    )
+  isUserExist.password = hashedPassword;
 
-    isUserExist.password = hashedPassword;
-
-    await isUserExist.save()
-}
+  await isUserExist.save();
+};
 const forgotPassword = async (email: string) => {
-    const isUserExist = await User.findOne({ email });
+  const isUserExist = await User.findOne({ email });
 
-    if (!isUserExist) {
-        throw new AppError(httpStatus.BAD_REQUEST, "User does not exist")
-    }
-    if (!isUserExist.isVerified) {
-        throw new AppError(httpStatus.BAD_REQUEST, "User is not verified")
-    }
-    if (isUserExist.isActive === IsActive.BLOCKED || isUserExist.isActive === IsActive.INACTIVE) {
-        throw new AppError(httpStatus.BAD_REQUEST, `User is ${isUserExist.isActive}`)
-    }
-    if (isUserExist.isDeleted) {
-        throw new AppError(httpStatus.BAD_REQUEST, "User is deleted")
-    }
+  if (!isUserExist) {
+    throw new AppError(httpStatus.BAD_REQUEST, "User does not exist");
+  }
+  if (!isUserExist.isVerified) {
+    throw new AppError(httpStatus.BAD_REQUEST, "User is not verified");
+  }
+  if (
+    isUserExist.isActive === IsActive.BLOCKED ||
+    isUserExist.isActive === IsActive.INACTIVE
+  ) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `User is ${isUserExist.isActive}`
+    );
+  }
+  if (isUserExist.isDeleted) {
+    throw new AppError(httpStatus.BAD_REQUEST, "User is deleted");
+  }
 
-    const jwtPayload = {
-        userId: isUserExist._id,
-        email: isUserExist.email,
-        role: isUserExist.role
-    }
+  const jwtPayload = {
+    userId: isUserExist._id,
+    email: isUserExist.email,
+    role: isUserExist.role,
+  };
 
-    const resetToken = jwt.sign(jwtPayload, envVars.JWT_ACCESS_SECRET, {
-        expiresIn: "10m"
-    })
+  const resetToken = jwt.sign(jwtPayload, envVars.JWT_ACCESS_SECRET, {
+    expiresIn: "10m",
+  });
 
-    const resetUILink = `${envVars.FRONTEND_URL}/reset-password?id=${isUserExist._id}&token=${resetToken}`
+  const resetUILink = `${envVars.FRONTEND_URL}/reset-password?id=${isUserExist._id}&token=${resetToken}`;
 
-    sendEmail({
-        to: isUserExist.email,
-        subject: "Password Reset",
-        templateName: "forgetPassword",
-        templateData: {
-            name: isUserExist.name,
-            resetUILink
-        }
-    })
+  sendEmail({
+    to: isUserExist.email,
+    subject: "Password Reset",
+    templateName: "forgetPassword",
+    templateData: {
+      name: isUserExist.name,
+      resetUILink,
+    },
+  });
 
-    /**
-     * http://localhost:5173/reset-password?id=687f310c724151eb2fcf0c41&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2ODdmMzEwYzcyNDE1MWViMmZjZjBjNDEiLCJlbWFpbCI6InNhbWluaXNyYXI2QGdtYWlsLmNvbSIsInJvbGUiOiJVU0VSIiwiaWF0IjoxNzUzMTY2MTM3LCJleHAiOjE3NTMxNjY3Mzd9.LQgXBmyBpEPpAQyPjDNPL4m2xLF4XomfUPfoxeG0MKg
-     */
-}
+  /**
+   * http://localhost:5173/reset-password?id=687f310c724151eb2fcf0c41&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2ODdmMzEwYzcyNDE1MWViMmZjZjBjNDEiLCJlbWFpbCI6InNhbWluaXNyYXI2QGdtYWlsLmNvbSIsInJvbGUiOiJVU0VSIiwiaWF0IjoxNzUzMTY2MTM3LCJleHAiOjE3NTMxNjY3Mzd9.LQgXBmyBpEPpAQyPjDNPL4m2xLF4XomfUPfoxeG0MKg
+   */
+};
 const setPassword = async (userId: string, plainPassword: string) => {
-    const user = await User.findById(userId);
+  const user = await User.findById(userId);
 
-    if (!user) {
-        throw new AppError(404, "User not found");
-    }
+  if (!user) {
+    throw new AppError(404, "User not found");
+  }
 
-    if (user.password && user.auths.some(providerObject => providerObject.provider === "google")) {
-        throw new AppError(httpStatus.BAD_REQUEST, "You have already set you password. Now you can change the password from your profile password update")
-    }
+  if (
+    user.password &&
+    user.auths.some((providerObject) => providerObject.provider === "google")
+  ) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      "You have already set you password. Now you can change the password from your profile password update"
+    );
+  }
 
-    const hashedPassword = await bcryptjs.hash(
-        plainPassword,
-        Number(envVars.BCRYPT_SALT_ROUND)
-    )
+  const hashedPassword = await bcryptjs.hash(
+    plainPassword,
+    Number(envVars.BCRYPT_SALT_ROUND)
+  );
 
-    const credentialProvider: IAuthProvider = {
-        provider: "credentials",
-        providerId: user.email
-    }
+  const credentialProvider: IAuthProvider = {
+    provider: "credentials",
+    providerId: user.email,
+  };
 
-    const auths: IAuthProvider[] = [...user.auths, credentialProvider]
+  const auths: IAuthProvider[] = [...user.auths, credentialProvider];
 
-    user.password = hashedPassword
+  user.password = hashedPassword;
 
-    user.auths = auths
+  user.auths = auths;
 
-    await user.save()
+  await user.save();
+};
+const changePassword = async (
+  oldPassword: string,
+  newPassword: string,
+  decodedToken: JwtPayload
+) => {
+  const user = await User.findById(decodedToken.userId);
 
-}
-const changePassword = async (oldPassword: string, newPassword: string, decodedToken: JwtPayload) => {
+  const isOldPasswordMatch = await bcryptjs.compare(
+    oldPassword,
+    user!.password as string
+  );
+  if (!isOldPasswordMatch) {
+    throw new AppError(httpStatus.UNAUTHORIZED, "Old Password does not match");
+  }
 
-    const user = await User.findById(decodedToken.userId)
+  user!.password = await bcryptjs.hash(
+    newPassword,
+    Number(envVars.BCRYPT_SALT_ROUND)
+  );
 
-    const isOldPasswordMatch = await bcryptjs.compare(oldPassword, user!.password as string)
-    if (!isOldPasswordMatch) {
-        throw new AppError(httpStatus.UNAUTHORIZED, "Old Password does not match");
-    }
+  user!.save();
+};
 
-    user!.password = await bcryptjs.hash(newPassword, Number(envVars.BCRYPT_SALT_ROUND))
-
-    user!.save();
-
-
-}
-
-//user - login - token (email, role, _id) - booking / payment / booking / payment cancel - token 
+//user - login - token (email, role, _id) - booking / payment / booking / payment cancel - token
 
 export const AuthServices = {
-    // credentialsLogin,
-    getNewAccessToken,
-    changePassword,
-    setPassword,
-    forgotPassword,
-    resetPassword
-}
+  // credentialsLogin,
+  getNewAccessToken,
+  changePassword,
+  setPassword,
+  forgotPassword,
+  resetPassword,
+};
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,29 +1,20 @@
 import { Router } from "express";
-import { UserControllers } from "./user.controller";
-import { validateRequest } from "../../middlewares/validateRequest";
-import { createUserZodSchema, updateUserZodSchema } from "./user.validation";
 import { checkAuth } from "../../middlewares/checkAuth";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { UserControllers } from "./user.controller";
 import { Role } from "./user.interface";
+import { updateUserZodSchema } from "./user.validation";
 
-const router = Router();
-// create or register router 
-router.post(
-  "/register",
-  validateRequest(createUserZodSchema),
-  UserControllers.createUser
-);
-// get all user router
-router.get(
-  "/all-users",
-  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+const router = Router()
 
-  UserControllers.getAllUsers
-);
-// update user router
-router.patch(
-  "/:id",
-  validateRequest(updateUserZodSchema),
-  checkAuth(...Object.values(Role)),
-  UserControllers.updateUser
-);
-export const UserRoutes = router;
+
+
+router.post("/register",
+    // validateRequest(createUserZodSchema),
+    UserControllers.createUser)
+router.get("/all-users", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), UserControllers.getAllUsers)
+router.get("/me", checkAuth(...Object.values(Role)), UserControllers.getMe)
+router.get("/:id", checkAuth(...Object.values(Role)), UserControllers.getSingleUser)
+router.patch("/:id", validateRequest(updateUserZodSchema), checkAuth(...Object.values(Role)), UserControllers.updateUser)
+// /api/v1/user/:id
+export const UserRoutes = router
